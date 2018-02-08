2faec9bfc44935b3d603845a6f41f7cc0293cae3
Folm <dev@folm.io>
2018-02-08 00:16:06 +0000

    Disable trading dialog

--- src/qt/walletview.cpp
+++ src/qt/walletview.cpp
@@ -21,7 +21,7 @@
 #include "transactiontablemodel.h"
 #include "transactionview.h"
 #include "walletmodel.h"
-#include "tradingdialog.h"
+//#include "tradingdialog.h"
 
 #include "ui_interface.h"
 
@@ -43,7 +43,7 @@ WalletView::WalletView(QWidget* parent) : QStackedWidget(parent),
     overviewPage = new OverviewPage();
     explorerWindow = new BlockExplorer(this);
     transactionsPage = new QWidget(this);
-    tradingPage = new tradingDialog(this);
+    //tradingPage = new tradingDialog(this);
     QVBoxLayout* vbox = new QVBoxLayout();
     QHBoxLayout* hbox_buttons = new QHBoxLayout();
     transactionView = new TransactionView(this);
@@ -70,14 +70,14 @@ WalletView::WalletView(QWidget* parent) : QStackedWidget(parent),
     hbox_buttons->addWidget(exportButton);
     vbox->addLayout(hbox_buttons);
     transactionsPage->setLayout(vbox);
-    tradingPage->setLayout(vbox);
+   // tradingPage->setLayout(vbox);
 
     receiveCoinsPage = new ReceiveCoinsDialog();
     sendCoinsPage = new SendCoinsDialog();
 
     addWidget(overviewPage);
     addWidget(transactionsPage);
-    addWidget(tradingPage);
+  //  addWidget(tradingPage);
     addWidget(receiveCoinsPage);
     addWidget(sendCoinsPage);
     addWidget(explorerWindow);
