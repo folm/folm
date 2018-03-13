12e638bc3a8dc8a21bc2a39921a99d8801d91f5b
Folm <dev@folm.io>
2018-03-13 15:06:56 +0300

    Update 3.2.1

--- src/main.cpp
+++ src/main.cpp
@@ -1979,7 +1979,7 @@ void Misbehaving(NodeId pnode, int howmuch)
         return;
 
     state->nMisbehavior += howmuch;
-    int banscore = GetArg("-banscore", 10000);
+    int banscore = GetArg("-banscore", 100);
     if (state->nMisbehavior >= banscore && state->nMisbehavior - howmuch < banscore) {
         LogPrintf("Misbehaving: %s (%d -> %d) BAN THRESHOLD EXCEEDED\n", state->name, state->nMisbehavior - howmuch, state->nMisbehavior);
         state->fShouldBan = true;
